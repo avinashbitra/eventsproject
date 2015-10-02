a89756cc834edb2847ce0943eefe891e70194515
Avinash Bitra <abitra@wrberkley.com>
2015-10-02 14:12:20 -0500

    added keys and prd dispatcher servlet

---
+++ src/main/java/com/avinashbitra/events/controller/EmailController.java
@@ -0,0 +1,18 @@
+package com.avinashbitra.events.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by abitra on 10/2/2015.
+ */
+public class EmailController {
+
+
+
+  @RequestMapping(value = "/email")
+  public void sendEmail(){
+
+  }
+
+
+}
